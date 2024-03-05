c4d7bbc168c45c85beaf45cc540b533251ae8ec1
Masya26 <mazelocked@gmail.com>
2024-03-05 15:15:10 +0500

    ЗАЛИЛ ХУЙНЮ ЗАБИРАЙ

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang=“en”>
+<head>
+<meta charset=“UTF-8”>
+<title>Posts Page</title>
+<style>
+/* Здесь вы можете указать стили для элементов страницы */
+</style>
+</head>
+<body>
+<header>
+<!-- Здесь может быть логотип, навигация и т.п. -->
+</header>
+<main>
+    <section>
+        <!-- Здесь могут быть блоки с постами -->
+    </section>
+</main>
+
+<footer>
+    <!-- Здесь может быть копирайт, контакты и т.п. -->
+</footer>
+</body>
+</html>
