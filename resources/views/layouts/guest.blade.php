960dc09ee32821b5a9aafebfc9fc5333e359fe99
«Almaz-Burakanov» <almazbs@mail.ru>
2024-05-07 19:16:58 +0500

    Подключили tailwind

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,6 +8,8 @@
         <title>{{ config('app.name', 'Авторизация') }}</title>
 
         <!-- Fonts -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <!-- Scripts -->
