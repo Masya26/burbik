f0ec7480cbfda73ec3f7a8fef4da710518bd585c
Masya26 <mazelocked@gmail.com>
2024-05-09 20:40:28 +0500

    fix

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -45,7 +45,7 @@
                     <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>{{ $orders_count }}<sup style="font-size: 20px">%</sup></h3>
+                            <h3>{{ $orders_count }}<sup style="font-size: 20px"></sup></h3>
 
                             <p>Количество заказов</p>
                         </div>
